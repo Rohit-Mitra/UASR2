b9b3fe0cdde0ccc1308f5742763d39b5c4b40847
Rohit-Mitra <69960781+Rohit-Mitra@users.noreply.github.com>
2022-12-20 00:16:54 +0530

    Update imagestitching.py

--- prac/imagestitching.py
+++ prac/imagestitching.py
@@ -1,15 +1,29 @@
 import cv2
-#import numpy as np
+import os
+import numpy as np
+mainFolder = 'Images'
+myfolders = os.listdir(mainFolder)
+print(myfolders)
 
-image_paths = ['uttower_left.jpg','uttower_right.jpg']              #accepts a list of images , have to change to make it accept more than 2 
+image_paths = []   #declare a list of images
 imgs = [] 
 
-for i in range(len(image_paths)):                                   #loop runs as long as image_paths has an image, ie returns true
-	imgs.append(cv2.imread(image_paths[i]))
+for folder in myfolders:
+    path = mainFolder + '/' + folder
+    myList = os.listdir(path)
+    print(f'Total no of images detected {len(myList)}')
+    for imgN in myList:
+        image_paths.append(f'{path}/{imgN}')
+
+   
+
+           
+arr = np.array(image_paths)
+for i in range(len(arr)):                                   #loop runs as long as image_paths has an image, ie returns true
+	imgs.append(cv2.imread(arr[i]))
   
-  
-cv2.imshow('1',imgs[0])
-cv2.imshow('2',imgs[1])                                             #need to change for more than 2
+for i in range(len(arr)):
+    cv2.imshow(f'{i}',imgs[i])                                            #need to change for more than 2
 
 
 stitchy = cv2.Stitcher.create()
@@ -19,13 +33,12 @@ if dummy != cv2.STITCHER_OK:
   print("stitching ain't successful")                             #to check if the stitching procedure is successful 
                                                                   #.stitch() func returns a true value if stitching is succesful
 else:
-	print('Your Panorama is ready!!!')
+	print('Your stitched image is ready!!!')
 
 # final output
 cv2.imshow('final result',output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
 #reference: https://www.geeksforgeeks.org/opencv-panorama-stitching/
 #https://www.youtube.com/watch?v=Z846tkgl9-U - module 9 for image stitching
